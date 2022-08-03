8966ba7d0ba165093b196c82fc4830803e360454
prizoner627 <dushyantha.world@gmail.com>
2022-08-04 00:33:45 +0530

    dfg

--- samples/src/setup.sh
+++ samples/src/setup.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 git clone https://github.com/prizoner627/dockersec.git
+pip install -r requirements.txt
 apt update
 apt install nmap
-chmod -R +x dockersec/
-cd dockersec/
-pip install -r requirements.txt
+chmod -R +x /dockersec
